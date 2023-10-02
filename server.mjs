c90453b2eb56f9c53de398861a25dc5f59dd3194
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-10-02 15:37:22 +0500

    setup envoirment for chat app

---
+++ server.mjs
@@ -0,0 +1,24 @@
+import express from "express"
+import path from "path";
+import http from "http"
+import { Server } from "socket.io"
+
+const app = express();
+const PORT = 9000;
+const server = http.createServer(app);
+const io = new Server(server)
+
+// Socket connections
+
+io.on("connection", (socket) => {
+    socket.on("user_message", (message) => {
+        io.emit("message", message)
+    })
+})
+
+app.use(express.static(path.resolve('./public')));
+app.get('/', (req, res) => {
+    return res.sendFile('/public/index.html')
+})
+
+server.listen(PORT, () => console.log(`Server is started at PORT: ${PORT}`))
